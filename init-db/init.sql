71ac5e75e8698ed93f79a241d3817ff1f6626fcc
JiilanTj <jiilan3maret2008@gmail.com>
2026-02-17 18:28:04 +0700

    Update environment variables and database initialization for Gitea configuration

--- init-db/init.sql
+++ init-db/init.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE sso_db;
 CREATE DATABASE compro_db;
 
+-- Gitea database and user
+-- Password should match GITEA_DB_PASSWORD env var
 CREATE DATABASE gitea_db;
-CREATE USER gitea_user WITH ENCRYPTED PASSWORD 'Soleplayer0p3$';
+CREATE USER gitea_user WITH ENCRYPTED PASSWORD 'WaduhAngkongNangLai11221212';
 GRANT ALL PRIVILEGES ON DATABASE gitea_db TO gitea_user;
